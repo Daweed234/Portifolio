792a69444957a28ebc394e535cd1113d1801254a
David <ntwalidavid31@gmail.com>
2024-04-23 20:28:40 +0200

    no page added

---
+++ src/Pages/NoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Components/Navbar'
+
+const NoPage = () => {
+    return (
+        <>
+            <Navbar />
+            <div>NoPage</div>
+        </>
+    )
+}
+
+export default NoPage
