f881d98f74c6531065d74cb1b76e7c7d0bb97cd6
David <ntwalidavid31@gmail.com>
2024-04-23 20:33:03 +0200

    About us page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+
+const About = () => {
+  return (
+    <>
+    <Navbar />
+    <div>About</div>
+    </>
+  )
+}
+
+export default About
