aed96bbeda4dd37f3b9b1aa5db95ed46750dd207
David <ntwalidavid31@gmail.com>
2024-04-25 09:13:53 +0200

    Projects page added

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+
+const Projects = () => {
+    return (
+        <>
+            <Navbar />
+            {/* <div className='flex justify-center items-center flex-col'>
+        <p className='text-5xl font-semibold'></p>
+        <p></p>
+        <p>😮‍💨</p>
+    </div> */}
+            <div className='mt-32'>
+                <div className=' text-center'>
+                    <p className='text-6xl font-bold mb-3'>Guess what.....</p>
+                    <p className='text-lg font-semibold'>We are still working on it</p>
+                </div>
+                <p className='text-center text-8xl mb-3'>😮‍💨</p>
+            </div>
+        </>
+    )
+}
+
+export default Projects
