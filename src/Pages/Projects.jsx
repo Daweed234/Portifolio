27c586108df81eda85307c4b679cdcdde660b850
David <ntwalidavid31@gmail.com>
2024-05-21 09:39:05 +0200

    responsive added

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <p></p>
         <p>😮‍💨</p>
     </div> */}
-            <div className='py-32'>
+            <div className='py-32 md:ml-14 md:justify-center md:items-center'>
                 <div className=' text-center'>
                     <p className='text-6xl font-bold mb-3 md:text-3xl dark:text-white'>Guess what.....</p>
                     <p className='text-lg font-semibold dark:text-white'>We are still working on it</p>
